f420d070bca1bd5305784cc9350a75cc9a2ccfb5
Daniel Szpisjak <felin.arch@gmail.com>
2016-02-06 23:57:01 +0100

    add homepage

--- snx.rb
+++ snx.rb
@@ -1,5 +1,6 @@
 class Snx < Formula
   desc "SNX installer"
+  homepage "https://github.com/felin-arch/snx"
   url "https://github.com/felin-arch/snx/archive/0.0.7.tar.gz"
   sha256 "f207d18995e9fbd4898ce779abf287e9dffe946744c4a60d9d7fd9a4aa25d163"
 
